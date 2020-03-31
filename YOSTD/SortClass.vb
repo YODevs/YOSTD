ecd90f7ca1716e4276fe484cabff3019b9b8deb8
Amin Badin <aminlord96@gmail.com>
2020-04-01 01:04:53 +0430

    Sort Class Added

---
+++ YOSTD/SortClass.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.CompilerServices
+
+Public Class Sort
+
+
+#Region "BubbleSort"
+
+    Public Shared Function BubbleSort(YODA_F As String) As String
+        Dim items As ArrayList = YODA_Format.ReadYODA(YODA_F)
+        For i = 1 To (items.Count - 1)
+            For j = 0 To ((items.Count - 1) - i)
+                If items(j) > items(j + 1) Then
+                    items(j) = items(j) Xor items(j + 1)
+                    items(j + 1) = items(j + 1) Xor items(j)
+                    items(j) = items(j) Xor items(j + 1)
+                End If
+            Next
+        Next
+        Return YODA_Format.WriteYODA(items)
+    End Function
+
+#End Region
+
+
+
+End Class
