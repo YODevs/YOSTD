17f8c8488e8dce3ef7d74f9f6e5f7119e18307b8
Amin Badin <aminlord96@gmail.com>
2020-04-01 13:07:57 +0430

    CheckValidItems Method Added

--- YOSTD/SortClass.vb
+++ YOSTD/SortClass.vb
@@ -3,11 +3,18 @@ Imports Microsoft.VisualBasic.CompilerServices
 
 Public Class Sort
 
-
+    Private Shared Sub CheckVaildItems(items As ArrayList, referAlgorithm As String)
+        For index = 0 To items.Count - 1
+            If IsNumeric(items(index)) = False Then
+                Throw New Exception("The " & referAlgorithm & " sorting algorithm cannot use string['" & items(index) & "'].")
+            End If
+        Next
+    End Sub
 #Region "BubbleSort"
 
     Public Shared Function BubbleSort(YODA_F As String) As String
         Dim items As ArrayList = YODA_Format.ReadYODA(YODA_F)
+        CheckVaildItems(items, "Bubble")
         For i = 1 To (items.Count - 1)
             For j = 0 To ((items.Count - 1) - i)
                 If items(j) > items(j + 1) Then
@@ -21,7 +28,6 @@ Public Class Sort
     End Function
 
 #End Region
-
 
 
 End Class
