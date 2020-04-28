4729cb675260d54b63c849b4decfc288347ed6c1
Amin Badin <aminlord96@gmail.com>
2020-04-28 19:03:10 +0430

    ClipBoard Class Added.

---
+++ YOSTD/ClipboardAPI.vb
@@ -0,0 +1,26 @@
+﻿
+Imports System.Drawing
+
+Public Class ClipBoard
+
+
+    Public Shared Sub SetData(value As String)
+        My.Computer.Clipboard.SetText(value)
+    End Sub
+
+    Public Shared Function GetData() As String
+        If My.Computer.Clipboard.ContainsText = False Then Return Nothing
+        Return My.Computer.Clipboard.GetText()
+    End Function
+
+    Public Shared Sub ClearData()
+        My.Computer.Clipboard.Clear()
+    End Sub
+
+    Public ReadOnly Property containsData() As String
+        Get
+            Return My.Computer.Clipboard.ContainsText
+        End Get
+    End Property
+
+End Class
